73c3a70e3875a9d89b51642efadad8a694defa72
jeremy.teurterie <jeremy.teurterie@gmail.com>
2022-07-12 17:09:29 +0700

    :construction: feat: WIP form implementation
    Work in progress form implementation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,15 +12,66 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
-const form = document.getElementById("#signup"); // referencing form element
+const form = document.getElementById("signup"); // referencing form element
 
-// validations or errors messages
-const NAME_INVALID = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-const OPTION_INVALID = "Vous devez choisir une option.";
-const CONDITION_INVALID = "Vous devez vérifier que vous acceptez les termes et conditions.";
-const BIRTHDAY_INVALID = "Vous devez entrer votre date de naissance.";
-const EMAIL_INVALID = "Veuillez saisir un format d'adresse e-mail correct";
+form.addEventListener('submit', function(stopForm) {
+  let firstInput = document.getElementById("first");
+  let check = /^[a-zA-Z]{3,}$/;
+  
+    if (firstInput.value.trim() == "") {
+      let first_invalid = document.getElementById("first-invalid");
+      first_invalid.innerHTML = "Le champ prénom est requis.";
+      first_invalid.style.color = 'red';
+      stopForm.preventDefault();
+    } else if (check.test(firstInput.value) == false) {
+        let first_invalid = document.getElementById("first-invalid");
+        first_invalid.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+        first_invalid.style.color = 'red';
+        stopForm.preventDefault();
+    }
 
+  let lastInput = document.getElementById("last");
+    if (lastInput.value.trim() == "") {
+      let last_invalid = document.getElementById("last-invalid");
+      last_invalid.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      last_invalid.style.color = 'red';
+    }
+
+  let emailInput = document.getElementById("email");
+    if (emailInput.value.trim() == "") {
+      let email_invalid = document.getElementById("email-invalid");
+      email_invalid.innerHTML = "Veuillez saisir un format d'adresse e-mail correct.";
+      email_invalid.style.color = 'red';
+    }
+
+  let birthdateInput = document.getElementById("birthdate");
+    if (birthdateInput.value.trim() == "") {
+      let birthdate_invalid = document.getElementById("birthdate-invalid");
+      birthdate_invalid.innerHTML = "Vous devez entrer votre date de naissance.";
+      birthdate_invalid.style.color = 'red';
+    }
+
+  let quantityInput = document.getElementById("quantity");
+    if (quantityInput.value.trim() == "") {
+      let quantity_invalid = document.getElementById("quantity-invalid");
+      quantity_invalid.innerHTML = "Veuillez choisir une valeur entre 0 et 99.";
+      quantity_invalid.style.color = 'red';
+    }
+
+  let locationInput = document.getElementsByName("location");
+    if (locationInput.value.trim() == "") {
+      let location_invalid = document.getElementsByName("location");
+      location_invalid.innerHTML = "Vous devez choisir une option.";
+      location_invalid.style.color = 'red';
+    }
+
+  let checkboxInput = document.getElementById("checkbox1");
+    if (checkboxInput.value.trim() == "") {
+      let checkbox_invalid = document.getElementById("checkbox1");
+      checkbox_invalid.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+      checkbox_invalid.style.color = 'red';
+    }
+});
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
